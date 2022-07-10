df7cca811b8458bf397724cc51c3a6a985f6bd16
zhyunp <zhyunp1984@gmail.com>
2022-07-11 01:08:27 +0900

    logs 출력 로그 pybo 로거 등록

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -3,9 +3,11 @@ from ..models import Question
 from django.core.paginator import Paginator
 from django.contrib import messages
 from django.db.models import Q
+import logging
+logger = logging.getLogger('pybo')
 
 def index(request):
-    3 / 0  # 강제로 오류발생
+    logger.info("INFO 레벨로 출력")
     page = request.GET.get('page','1') # 페이지
     kw = request.GET.get('kw', '')  #검색어
     question_list = Question.objects.order_by('-create_date')
